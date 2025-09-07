7deacb7c9ff3fa5d6b76516cac2fe2e9d8cadc09
SeanOsorio <sosorior1@academia.usbbog.edu.co>
2025-09-07 11:04:41 -0500

    feat: cambio en variables para el mejor funcionamiento

--- config/database.py
+++ config/database.py
@@ -1,25 +1,31 @@
-
-
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.weapons_model import Base
 from dotenv import load_dotenv
 
-# Cargar variables de entorno desde .env
 load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+# Obtener los parámetros individuales de la base de datos
+
+DBUSER = os.getenv('DBUSER')
+DBPASSWORD = os.getenv('DBPASSWORD')
+DBHOST = os.getenv('DBHOST')
+DBPORT = os.getenv('DBPORT', '5432')
+DBNAME = os.getenv('DBNAME')
+
+# Construir la URL de conexión para PostgreSQL
+DATABASE_URL = f"postgresql://{DBUSER}:{DBPASSWORD}@{DBHOST}:{DBPORT}/{DBNAME}"
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
-	db = SessionLocal()
-	try:
-		yield db
-	finally:
-		db.close()
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 def init_db():
-	Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
